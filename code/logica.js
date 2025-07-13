e6e3b6f9d26b6cfcf62e4deac939696ea61379f1
Narizaico-code <areyes-2024435@kinal.edu.gt>
2025-07-13 01:03:41 -0600

    j

---
+++ code/logica.js
@@ -0,0 +1,54 @@
+function corazon1() {
+    const contenedor = document.getElementById("emoji");
+
+    const corazon = document.createElement("span");
+    corazon.innerText = "💖";
+    corazon.style.fontSize = "400px";
+    corazon.style.position = "absolute";
+    corazon.style.top = "10px";
+    corazon.style.left = "50%";
+    corazon.style.transform = "translateX(-50%)";
+    corazon.style.zIndex = "1000";
+
+    contenedor.appendChild(corazon);
+
+    setTimeout(() => {
+        window.location.href = "/code/flaquitamasmegusta.html";
+    }, 50);
+}
+function corazon2() {
+    const contenedor = document.getElementById("emoji");
+
+    const corazon = document.createElement("span");
+    corazon.innerText = "💖";
+    corazon.style.fontSize = "400px";
+    corazon.style.position = "absolute";
+    corazon.style.top = "10px";
+    corazon.style.left = "50%";
+    corazon.style.transform = "translateX(-50%)";
+    corazon.style.zIndex = "1000";
+
+    contenedor.appendChild(corazon);
+
+    setTimeout(() => {
+        window.location.href = "/code/flaquitamasmegusta.html";
+    }, 50);
+}
+function corazon3() {
+    const contenedor = document.getElementById("emoji");
+
+    const corazon = document.createElement("span");
+    corazon.innerText = "💖";
+    corazon.style.fontSize = "400px";
+    corazon.style.position = "absolute";
+    corazon.style.top = "10px";
+    corazon.style.left = "50%";
+    corazon.style.transform = "translateX(-50%)";
+    corazon.style.zIndex = "1000";
+
+    contenedor.appendChild(corazon);
+
+    setTimeout(() => {
+        window.location.href = "/code/flaquitamasmegusta.html";
+    }, 50);
+} 
